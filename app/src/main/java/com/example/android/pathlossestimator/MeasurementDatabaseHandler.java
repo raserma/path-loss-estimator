bc9502c7b9ae7091645f3d09ecf6fa657671bae2
raserma <raserma91@gmail.com>
2015-01-31 15:24:17 +0100

    First commit, measurement gathering done

---
+++ app/src/main/java/com/example/android/pathlossestimator/MeasurementDatabaseHandler.java
@@ -0,0 +1,187 @@
+package com.example.android.pathlossestimator;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+
+public class MeasurementDatabaseHandler extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "measurements";
+    private static final String TABLE_BSSIDS = "bssids";
+    private static final String TABLE_MEASUREMENTS = "measurements";
+
+    // bssids table columns names
+    private static final String KEY_BSSID_ID = "id";
+    private static final String KEY_BSSID_NAME = "name";
+
+    // measurements table column names
+    private static final String KEY_MEASUREMENT_ID = "id";
+    private static final String KEY_BSSID = "id_bssid";
+    private static final String KEY_RSS = "value_rss";
+    private static final String KEY_DISTANCE = "value_distance";
+
+
+
+    // AP MAC ADDRESS LIST
+    private static final String BSSID1 = "00:17:0f:d9:71:d";
+    private static final String BSSID2 = "00:1b:0a:d9:71:d"; // Wrong MAC
+    private static final String BSSID3 = "00:17:0f:d9:6f:d";
+    private static final String BSSID4 = "f4:7f:35:f6:ab:a";
+    private static final String BSSID5 = "18:33:9d:fe:9c:6";
+    private static final String BSSID6 = "18:33:9d:fe:91:c";
+    private static final String BSSID7 = "18:33:9d:f9:31:8";
+    private static final String BSSID8 = "18:33:9d:f9:84:7";
+    private static final String BSSID9 = "04:da:d2:a7:2f:c";
+    private static final String BSSID10 = "04:da:d2:29:bf:8";
+    private static final String BSSID11 = "04:da:d2:57:0a:3";
+    private static final String BSSID12 = "04:da:d2:29:c4:c";
+    private static final String BSSID13 = "04:da:d2:57:0a:3";
+    private static final String BSSID14 = "04:da:d2:56:ee:e";
+    private static final String BSSID15 = "04:da:d2:29:c2:3";
+    private static final String BSSID16 = "04:da:d2:57:0d:a";
+    private static final String BSSID17 = "04:da:d2:29:b4:0";
+    private static final String BSSID18 = "04:da:d2:57:0e:5";
+
+
+    /**
+     * CONSTRUCTORS
+     */
+    public MeasurementDatabaseHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    /**
+     * INSTANCE METHODS
+     */
+
+    /** Only runs once, when there is no database in the system */
+
+    public void onCreate(SQLiteDatabase db) {
+
+        // create bssids table
+        String CREATE_BSSID_TABLE = "CREATE TABLE " + TABLE_BSSIDS + "("
+                + KEY_BSSID_ID + " INTEGER PRIMARY KEY,"
+                + KEY_BSSID_NAME + " TEXT" + ")";
+        db.execSQL(CREATE_BSSID_TABLE);
+
+        // fills bssids table
+        String [] bssids = {
+                BSSID1, BSSID2, BSSID3, BSSID4, BSSID5, BSSID6, BSSID7, BSSID8, BSSID9, BSSID10,
+                BSSID11, BSSID12, BSSID13, BSSID14, BSSID15, BSSID16, BSSID17, BSSID18
+
+        };
+        ContentValues bssidValues = new ContentValues();
+        for (int i = 1; i < bssids.length + 1; i++) {
+            bssidValues.put(KEY_BSSID_NAME, bssids[i-1]);
+            // insert bssid names into bssids table
+            db.insert(TABLE_BSSIDS, null, bssidValues);
+        }
+
+        // create measurements table
+        String CREATE_MEASUREMENTS_TABLE = "CREATE TABLE " + TABLE_MEASUREMENTS + "("
+                + KEY_MEASUREMENT_ID + " INTEGER PRIMARY KEY,"
+                + KEY_BSSID + " INTEGER,"
+                + KEY_RSS + " INTEGER,"
+                + KEY_DISTANCE + " INTEGER" + ")";
+        db.execSQL(CREATE_MEASUREMENTS_TABLE);
+    }
+
+    /**
+     * getBssidNameDB
+     * Gets MAC/BSSID address which corresponds to id_BSSID
+     *
+     * @param id_BSSID Identification number of currently used AP MAC/BSSID
+     */
+    public String getBssidNameDB(int id_BSSID) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_BSSIDS, new String[]{KEY_BSSID_NAME}, KEY_BSSID_ID + "=?",
+                new String[]{String.valueOf(id_BSSID)}, null, null, null, null);
+
+        String bssid = "";
+        if (cursor.moveToFirst()) {
+            bssid = cursor.getString(0);
+        }
+        db.close();
+        return bssid;
+
+    }
+
+    public void addMeasurementDB(int id_BSSID, int value_RSS, int value_distance) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues measurementValues = new ContentValues();
+        measurementValues.put(KEY_BSSID, id_BSSID);
+        measurementValues.put(KEY_RSS, value_RSS);
+        measurementValues.put(KEY_DISTANCE, value_distance);
+
+        // insert AP measurements into measurements table
+        db.insert(TABLE_MEASUREMENTS, null, measurementValues);
+        db.close();
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_BSSIDS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_MEASUREMENTS);
+
+        // Create tables again
+        onCreate(db);
+    }
+
+
+
+
+
+
+    public ArrayList<Cursor> getData(String Query) {
+        //get writable database
+        SQLiteDatabase sqlDB = this.getWritableDatabase();
+        String[] columns = new String[]{"mesage"};
+        //an array list of cursor to save two cursors one has results from the query
+        //other cursor stores error message if any errors are triggered
+        ArrayList<Cursor> alc = new ArrayList<Cursor>(2);
+        MatrixCursor Cursor2 = new MatrixCursor(columns);
+        alc.add(null);
+        alc.add(null);
+
+
+        try {
+            String maxQuery = Query;
+            //execute the query results will be save in Cursor c
+            Cursor c = sqlDB.rawQuery(maxQuery, null);
+
+
+            //add value to cursor2
+            Cursor2.addRow(new Object[]{"Success"});
+
+            alc.set(1, Cursor2);
+            if (null != c && c.getCount() > 0) {
+
+
+                alc.set(0, c);
+                c.moveToFirst();
+
+                return alc;
+            }
+            return alc;
+        } catch (Exception ex) {
+
+            Log.d("printing exception", ex.getMessage());
+
+            //if any exceptions are triggered save the error message to cursor an return the arraylist
+            Cursor2.addRow(new Object[]{"" + ex.getMessage()});
+            alc.set(1, Cursor2);
+            return alc;
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/android/pathlossestimator/StartActivity.java
@@ -0,0 +1,52 @@
+package com.example.android.pathlossestimator;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class StartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_start, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void openDataMeasuringActivity(View view){
+        Intent intentMeasurementScreen = new Intent(this, MeasuringDataActivity.class);
+        startActivity(intentMeasurementScreen);
+    }
+
+    public void openPathLossEstimationActivity(View view){
+        Intent intentEstimationScreen = new Intent(this, MeasuringDataActivity.class);
+        startActivity(intentEstimationScreen);
+    }
+}
